dd4db8405e8e577ff6ff82e1aab60045ed23e587
Peter Jones <pwjones76@gmail.com>
2018-01-17 10:16:14 -0500

    Fixed subplot pete to remotely work

--- subplot_pete.m
+++ subplot_pete.m
@@ -1,16 +1,17 @@
 function [ah, fh] = subplot_pete(rows, cols, xlabeltxt, ylabeltxt, titletxt)
 
-nplot = rows*cols;
+%nplot = rows*cols;
 jj=1;
-for r = 1:rows
-    for c=1:cols
-        ah(jj) = axes('Position', [.05*c+(.8/ncol*(c-1)) .9*(.05+.02*r+((r-1)*.75/nrow)) .85/ncol .9*(.75/nrow)]);
+fh = figure;
+for c=1:cols
+    for r = rows:-1:1
+        ah(jj) = axes('Position', [.05*c+(.8/cols*(c-1)) .9*(.05+.02*r)+((r-1)*.75/rows) .85/cols .9*(.75/rows)]);
         set(gca, 'TickDir','out');
-        xlabel(xlabeltxt);
-        ylabel(ylabeltxt);
+        if r==1 xlabel(xlabeltxt); end
+        if c==1 ylabel(ylabeltxt); end
+        jj = jj+1;
     end
 end
 titleax = axes('Position', [0 0 1 1], 'Visible', 'off');
-fh = gcf;
 text(.1, .9, titletxt, 'FontSize', 16);
        
