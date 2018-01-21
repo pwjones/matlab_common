beab02290c4fffc3048654b1fd9a64e001be326e
Peter Jones <pwjones76@gmail.com>
2018-01-21 12:00:24 -0500

    fixed subplot_pete's positioning of axes vertically.  Broke with large numbers before.

--- subplot_pete.m
+++ subplot_pete.m
@@ -3,9 +3,11 @@ function [ah, fh] = subplot_pete(rows, cols, xlabeltxt, ylabeltxt, titletxt)
 %nplot = rows*cols;
 jj=1;
 fh = figure;
+rowh = .8/rows;
+colw = .9/cols;
 for c=1:cols
     for r = rows:-1:1
-        ah(jj) = axes('Position', [.05*c+(.8/cols*(c-1)) .9*(.05+.02*r)+((r-1)*.75/rows) .85/cols .9*(.75/rows)]);
+        ah(jj) = axes('Position', [(colw*(c-1)+.075) .05+rowh*(r-1) .8*colw .9*rowh]);
         set(gca, 'TickDir','out');
         if r==1 xlabel(xlabeltxt); end
         if c==1 ylabel(ylabeltxt); end
@@ -13,5 +15,5 @@ for c=1:cols
     end
 end
 titleax = axes('Position', [0 0 1 1], 'Visible', 'off');
-text(.1, .9, titletxt, 'FontSize', 16);
+text(.1, .95, titletxt, 'FontSize', 16);
        
